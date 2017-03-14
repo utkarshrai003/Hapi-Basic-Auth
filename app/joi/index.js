b02d3c669d84612c14487bfd38c3f472a6d617af
utkarsh <utkarshrai003@gmail.com>
2017-03-14 11:04:51 +0530

    Added index.js file for requiring all the JOI files

--- app/joi/index.js
+++ app/joi/index.js
@@ -1,4 +1,8 @@
 
 module.exports = {
-  userCreate: require('./user/create')
+  userCreate: require('./user/create'),
+  userSignIn: require('./user/signIn'),
+  userResetPassword: require('./user/resetPassword'),
+  userForgetPassword: require('./user/forgetPassword'),
+  userRecoverPassword: require('./user/recoverPassword')
 }
