00f94816a5bdf336bb160ea23a68136104a3a804
utkarsh <utkarshrai003@gmail.com>
2017-03-14 11:03:12 +0530

    Added JOI schema validation for creating a user record

--- app/joi/user/create.js
+++ app/joi/user/create.js
@@ -4,5 +4,5 @@ var Joi = require('joi');
 module.exports = Joi.object().keys({
   name: Joi.string().required().min(6).max(12),
   email: Joi.string().email().required(),
-  password: Joi.string().alphanum().min(6).max(10).required()
+  password: Joi.string().alphanum().min(6).max(12).required()
 });
