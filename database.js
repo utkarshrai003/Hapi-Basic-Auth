b9af15c54fd27db2606e78f6b919eccf2beab65a
utkarsh <utkarshrai003@gmail.com>
2017-03-15 11:14:26 +0530

    Added Remote databse url

--- database.js
+++ database.js
@@ -2,7 +2,9 @@
 const Mongoose = require('mongoose');
 const config = require('./config');
 
-Mongoose.connect('mongodb://' + config.database.host + '/' + config.database.db);
+// Database hosted on mlab.com
+Mongoose.connect('mongodb://utkarsh:password@ds131480.mlab.com:31480/hapi-auth');
+//Mongoose.connect('mongodb://' + config.database.host + '/' + config.database.db);
 var db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
