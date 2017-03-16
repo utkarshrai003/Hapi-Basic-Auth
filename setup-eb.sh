19a6cb28746365c72c3670e06b17d63a9088b714
utkarsh <utkarshrai003@gmail.com>
2017-03-16 13:19:27 +0530

    Changed the name of the aws profile

--- setup-eb.sh
+++ setup-eb.sh
@@ -21,6 +21,6 @@ touch /home/ubuntu/.aws/config
 chmod 600 /home/ubuntu/.aws/config
 
 # Creating Profile
-echo "[profile eb-cli]" >> /home/ubuntu/.aws/config
+echo "[profile myProfile]" >> /home/ubuntu/.aws/config
 echo "aws_access_key_id" = $AWS_ACCESS_KEY_ID >> /home/ubuntu/.aws/config
-echo "aws_secret_Access_key" = $AWS_SECRET_ACCESS_KEY >> /home/ubuntu/.aws/config
+echo "aws_secret_access_key" = $AWS_SECRET_KEY >> /home/ubuntu/.aws/config
