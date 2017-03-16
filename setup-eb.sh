d2b0a6170cd89526e4f9c9481c7a4e64e179f073
utkarsh <utkarshrai003@gmail.com>
2017-03-16 12:17:40 +0530

    Added script file for eb setup

---
+++ setup-eb.sh
@@ -0,0 +1,26 @@
+
+set -x
+set -e
+
+# Move to root directory of the current User
+# cd
+
+# # Getting zipped EB command line tool
+# wget https://s3.amazonaws.com/elasticbeanstalk/cli/AWS-ElasticBeanstalk-CLI-2.6.3.zip -O aws-cli-2.6.3
+#
+# # unzipping
+# unzip aws-cli-2.6.3
+
+# create aws directory
+mkdir /home/ubuntu/.aws
+
+# create eb profile config file
+touch /home/ubuntu/.aws/config
+
+# give permission
+chmod 600 /home/ubuntu/.aws/config
+
+# Creating Profile
+echo "[profile eb-cli]" >> /home/ubuntu/.aws/config
+echo "aws_access_key_id" = $AWS_ACCESS_KEY_ID >> /home/ubuntu/.aws/config
+echo "aws_secret_Access_key" = $AWS_SECRET_ACCESS_KEY >> /home/ubuntu/.aws/config
